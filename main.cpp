1d709c76e1dc66101108ecb3e44ae227c1497fd6
ledacvien <53806253+ledacvien@users.noreply.github.com>
2021-04-11 09:45:18 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    cin >> n >> k;
+    for(int i = 1; i <= n; ++i) cin >> a[i];
+
+    sort(a+1, a+n+1);
+
+    for(int i = 1; i <= n; ++i)
+    {
+
+    }
+    return 0;
+}
