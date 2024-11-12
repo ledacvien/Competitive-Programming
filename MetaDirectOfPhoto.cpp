fd50d00379ea0db36b8ee332c9739f57f38e966e
Dac Vien Le <53806253+ledacvien@users.noreply.github.com>
2024-11-12 15:18:36 -0700

    direct of photo hard ver

--- MetaDirectOfPhoto.cpp
+++ MetaDirectOfPhoto.cpp
@@ -44,28 +44,131 @@ int getArtisticPhotographCount(int N, string C, int X, int Y) {
 	return result;
 }
 
+// Hard version
+// 1 <= N <= 300,000
+// 1 <= X <= Y <= N
+// Algo: precount the B, and P
+//  artistic photo always in format PAB or BAP
+//		0 1 2 3 4
+//		A P A B A
+//pB 	0 0 0 0 1 0
+//pP 	0 0 1 1 1 1
+long long getArtisticPhotographCountH(int N, string C, int X, int Y) {
+	vector<int> prefixB(N+1, 0);
+	vector<int> prefixP(N+1, 0);
+	vector<int> posA;
+	long long result = 0;
+
+	for (int i = 0; i < N; ++i)
+	{
+		prefixB[i+1] = prefixB[i] + (C[i] == 'B' ? 1 : 0);
+		prefixP[i+1] = prefixP[i] + (C[i] == 'P' ? 1 : 0);
+		if (C[i] == 'A')
+			posA.push_back(i);
+	}
+
+	for (int i : posA)
+	{
+		int leftX = max(0, i-X+1);
+		int leftY = max(0, i-Y);
+		int rightX = min(N, i+X);
+		int rightY = min(N, i+Y+1);
+		// calculate number of BAP
+		result += 1LL * (prefixB[leftX] - prefixB[leftY]) * (prefixP[rightY] - prefixP[rightX]);
+
+		// calculate number of PAB
+		result += 1LL * (prefixP[leftX] - prefixP[leftY]) * (prefixB[rightY] - prefixB[rightX]);
+	}
+
+	return result;
+}
+
+void stresstest() {
+	int correct = 0;
+	for (int t = 0; t < 10000; ++t)
+	{
+		string s = "";
+		int c;
+		int N = rand() % 1000 + 1;
+		for (int i = 0; i < N; ++i)
+		{
+			// random 0, 1, 2, 3
+			c = rand() % 6;
+			switch (c){
+			case 0: 
+				s += 'P';
+				break;
+			case 1: 
+				s += 'A';
+				break;
+			case 2:
+				s += 'B';
+				break;
+			case 3:
+				s += '.';
+				break;
+			// over sampling B, P
+			case 4:
+				s += 'P';
+				break;
+			case 5:
+				s += 'B';
+				break;
+			}
+		}
+		int X = rand() % 10 + 1;
+		int Y = rand() % 100 + 1;
+		int expected, actual;
+		if (X <= Y) {
+			expected = getArtisticPhotographCount(N, s, X, Y);
+			actual = getArtisticPhotographCountH(N, s, X, Y);
+		} else {
+			expected = getArtisticPhotographCount(N, s, Y, X);
+			actual = getArtisticPhotographCountH(N, s, Y, X);
+		}
+
+		if (expected != actual) {
+			cout << s << '\n' << N << ' ' << X << ' ' << Y << endl;
+			printf("expected: %d \nactual: %d\n", expected, actual);
+		} else {
+			correct++;
+		}
+	}
+
+	cout << correct << endl;
+	
+}
+
 int test1() {
 	int N = 5, X = 1, Y = 2;
 	string C = "APABA";
-	return getArtisticPhotographCount(N, C, X, Y);
+	return getArtisticPhotographCountH(N, C, X, Y);
 }
 
 int test2() {
 	int N = 5, X = 2, Y = 3;
 	string C = "APABA";
-	return getArtisticPhotographCount(N, C, X, Y);
+	return getArtisticPhotographCountH(N, C, X, Y);
 }
 
 int test3() {
 	int N = 8, X = 1, Y = 3;
 	string C = ".PBAAP.B";
+	return getArtisticPhotographCountH(N, C, X, Y);
+}
+
+int test4() {
+	int N = 20, X = 6, Y = 9;
+	string C = "BPAPBABAPBBAAPPB";
 	return getArtisticPhotographCount(N, C, X, Y);
 }
 
 int main() {
 
-	cout << test1() << endl;
-	cout << test2() << endl;
-	cout << test3() << endl;
+	// cout << test1() << endl;
+	// cout << test2() << endl;
+	// cout << test3() << endl;
+	//cout << test4() << endl;
+	stresstest();
 	return 0;
 }
